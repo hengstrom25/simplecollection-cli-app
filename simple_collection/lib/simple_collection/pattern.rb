d8889069c0f534fa6b9c36d77f6269121dbfc2b0
Heidi Engstrom <38007070+hengstrom25@users.noreply.github.com>
2018-07-05 21:06:55 -0400

    Update pattern.rb
    remove class variable @@all - not needed

--- simple_collection/lib/simple_collection/pattern.rb
+++ simple_collection/lib/simple_collection/pattern.rb
@@ -2,16 +2,9 @@ class Pattern
 
   attr_accessor :name, :url, :size, :materials, :gauge, :suggested_needles, :notes, :description
 
-  @@all = []
-
   def initialize(name=nil, url=nil)
     @name = name
     @url = url
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
   
   def print_pattern
