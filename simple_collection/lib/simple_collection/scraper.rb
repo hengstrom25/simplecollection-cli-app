1bb35c382dac597843c635112e22cb276d1f1a42
Heidi Engstrom <38007070+hengstrom25@users.noreply.github.com>
2018-07-05 20:59:57 -0400

    Update scraper.rb
    Move scrape_pattern from pattern.rb

--- simple_collection/lib/simple_collection/scraper.rb
+++ simple_collection/lib/simple_collection/scraper.rb
@@ -10,6 +10,16 @@ class Scraper
       patt.name != "Handmade in the UK" && patt.name != "Knitting Basics PDF" && patt.name != "Grain by Tin Can Knits"}
     #puts doc.css("tr p:first-child span.pattnavtext")
   end
+  
+  def scrape_pattern(pattern)
+    doc = Nokogiri::HTML(open("http://tincanknits.com/#{pattern.url}"))
+    pattern.size = doc.css("div.LHbox table")[1].css("tr")[0].css("td")[1].text
+    pattern.materials = doc.css("div.LHbox table")[1].css("tr")[1].css("td")[1].text
+    pattern.gauge = doc.css("div.LHbox table")[1].css("tr")[2].css("td")[1].text
+    pattern.suggested_needles = doc.css("div.LHbox table")[1].css("tr")[3].css("td")[1].text
+    pattern.notes = doc.css("div.LHbox table")[1].css("tr")[4].css("td")[1].text
+    pattern.description = doc.css("div.LHbox blockquote p").text
+  end
 
 
 end
